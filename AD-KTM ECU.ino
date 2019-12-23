d61be53a8ffe82a76e3ade8db7886acd0307f036
Junix <Junix@JUNIX-PC>
2019-12-23 14:27:37 +0800

    Add project files.

---
+++ AD-KTM ECU.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AD_KTM_ECU.ino
+ Created:	12/23/2019 2:26:39 PM
+ Author:	Junix
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
